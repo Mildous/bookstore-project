ae8ba9095fd6c9d619c68b02a18736f2987d3c15
Mildous <9089_@naver.com>
2022-12-26 18:43:38 +0900

    Initial commit

---
+++ src/main/java/com/mildous/bookstoreproject/BookstoreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mildous.bookstoreproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mildous/bookstoreproject/BookstoreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mildous.bookstoreproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
